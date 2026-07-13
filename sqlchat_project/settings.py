341d3f42055d3230ee2b6e592d9820c09d785a67
kyangwi <joshkyangwi@gmail.com>
2026-07-13 10:49:04 +0200

    add CSRF trusted origins for railway

--- sqlchat_project/settings.py
+++ sqlchat_project/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://*.up.railway.app",
+]
+
 
 # Application definition
 
